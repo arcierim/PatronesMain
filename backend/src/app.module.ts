6614ddbdd70ae4c6605622b18f9714d9e831bd46
Jhoreinis Anaya <91376980+Jhoreinis@users.noreply.github.com>
2025-08-21 23:05:42 -0500

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,1 +1,8 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [AuthModule], // Importamos el módulo de autenticación
+})
+export class AppModule {}
 
