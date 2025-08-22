0945ad664c9b1293d358d57cf3ae22ba5d0eac1f
Jhoreinis Anaya <91376980+Jhoreinis@users.noreply.github.com>
2025-08-21 23:03:23 -0500

    Update auth.controller.ts

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,1 +1,25 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import { PrismaService } from '../prisma.service';
+
+@Controller('auth')
+export class AuthController {
+  private authService: AuthService;
+
+  constructor(private prisma: PrismaService) {
+    this.authService = AuthService.getInstance(prisma);
+  }
+
+  @Post('register')
+  async register(@Body() data: RegisterDto) {
+    return this.authService.registerUser(data);
+  }
+
+  @Post('login')
+  async login(@Body() data: LoginDto) {
+    return this.authService.validateUser(data.email, data.password);
+  }
+}
 
