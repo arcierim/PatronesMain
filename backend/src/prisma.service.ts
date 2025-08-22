e8e7722ef349b0eba879414b37c13a78e78213d2
Jhoreinis Anaya <91376980+Jhoreinis@users.noreply.github.com>
2025-08-21 22:59:40 -0500

    Update prisma.service.ts

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -1,1 +1,27 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private static instance: PrismaService;
+
+  private constructor() {
+    super();
+  }
+
+  static getInstance(): PrismaService {
+    if (!PrismaService.instance) {
+      PrismaService.instance = new PrismaService();
+    }
+    return PrismaService.instance;
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
 
